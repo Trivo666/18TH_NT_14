d275c6dadc9d3b2b32bf1129b800084d41912de3
Trivo666 <86337398+Trivo666@users.noreply.github.com>
2022-03-02 14:10:11 +0700

    commit

--- DemoLogin/DemoLogin/Form1.Designer.cs
+++ DemoLogin/DemoLogin/Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace DemoLogin
         {
             this.lbluser = new System.Windows.Forms.Label();
             this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbluser
@@ -50,12 +52,33 @@ namespace DemoLogin
             this.txtUser.Size = new System.Drawing.Size(145, 20);
             this.txtUser.TabIndex = 1;
             // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(186, 104);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(49, 14);
+            this.lblPass.TabIndex = 0;
+            this.lblPass.Text = "Password";
+            this.lblPass.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(262, 104);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(145, 20);
+            this.txtPass.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(778, 297);
+            this.Controls.Add(this.txtPass);
             this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblPass);
             this.Controls.Add(this.lbluser);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -68,6 +91,8 @@ namespace DemoLogin
 
         private System.Windows.Forms.Label lbluser;
         private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtPass;
     }
 }
 
